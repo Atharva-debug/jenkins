ac795ad241e1f4e91a4e28ce579f9fc2679b2e50
Atharva-debug <65455553+Atharva-debug@users.noreply.github.com>
2023-02-17 01:50:28 +0530

    Create main. java

---
+++ main.java
@@ -0,0 +1,7 @@
+public class Main
+{
+    public static void main(String args[])
+    {
+          System.out.println("this file is updated and will be automatically pulled by jenkins");
+     }
+ }
